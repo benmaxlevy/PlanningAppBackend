34f44ec6c025cc449181b4d7d38ffd59127ae94f
Ben Levy <general@simplexwebsites.com>
2022-07-30 12:09:15 -0400

    Finish login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,12 @@
 const crypto = require("crypto");
 const { DataTypes } = require("sequelize");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: `${__dirname}/../../.env` });
 const User = require("../models/user");
 const { returnId: successId } = require("../lib/successMessages");
+const { http401: errorHttp401,
+    http401
+} = require("../lib/errorMessages");
 const { sequelize } = require("../models/index");
 
 exports.login = async (req, res, next) => {
@@ -21,8 +26,23 @@ exports.login = async (req, res, next) => {
                 password
             }
         });
-        // for "EDA"
-        console.log(loginUser.dataValues);
+
+        if (loginUser === null) {
+            return res.status(401).send(http401);
+        }
+
+        // create JWT with JWS
+        const userJwt = jwt.sign(
+            {
+                username: loginUser.dataValues.username,
+                email: loginUser.dataValues.email,
+                id: loginUser.dataValues.id
+            },
+            process.env.SECRET,
+            { expiresIn: 60*60 }
+        );
+
+        return res.status(200).send(userJwt);
     } catch (e) {
         return next(e);
     }
--- src/lib/errorMessages.js
+++ src/lib/errorMessages.js
@@ -1,7 +1,11 @@
 exports.generic = {
-    message: "An error occurred while handling the request",
+    message: "An error occurred while handling the request"
+};
+
+exports.http401 = {
+    message: "Access denied. Check username and password."
 };
 
 exports.http404 = {
-    message: "Unknown page.",
+    message: "Unknown page."
 };
